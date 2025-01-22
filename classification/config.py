28e47b9f32397e673660f07c35b8c8f2a9980db2
SelenaGeRuiqi <sege@ucsd.edu>
2025-01-22 15:13:03 -0800

    message

--- classification/config.py
+++ classification/config.py
@@ -12,7 +12,3 @@ finetune_mlp_epoch = {'SetFit/sst2': 30, 'ag_news': 5, 'yelp_polarity': 3, 'dbpe
 concept_set = {'SetFit/sst2': concepts.sst2, 'yelp_polarity': concepts.yelpp, 'ag_news': concepts.agnews, 'dbpedia_14': concepts.dbpedia}
 #cbl_epochs = {'SetFit/sst2': 30, 'ag_news': 5, 'yelp_polarity': 3, 'dbpedia_14': 3}
 cbl_epochs = {'SetFit/sst2': 10, 'ag_news': 3, 'yelp_polarity': 2, 'dbpedia_14': 2}
-
-'''
-this is a test
-'''
